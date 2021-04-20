0c1ae369d4bd9cd2a063bb06d4ebcc9a138aca6a
ostrovamit <82804444+ostrovamit@users.noreply.github.com>
2021-04-20 16:17:03 +0300

    end of day one

---
+++ register.js
@@ -0,0 +1,25 @@
+
+function submitDetils(){
+    alert("I am an alert box!");
+    checkPassword()
+    var form = document.getElementById("registerForm");
+    return
+}
+
+function checkPassword(){
+    var letterNumber = /^[0-9a-zA-Z]+$/;
+    try { 
+
+        if ($("#Password").val().length < 6){
+            throw "password must be more than 6 charachters";
+        }
+        else if ($("#Password").val().match(letterNumber)){
+            throw "password must contain letters and numbers";
+
+        }
+    }
+    catch(err) {
+        message.innerHTML = "Input is " + err;
+    }
+    
+}
