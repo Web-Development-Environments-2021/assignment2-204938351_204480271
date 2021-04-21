1a7bdb839b27e35b343afd6d9414081e2ebd55ed
noaakl <62835962+noaakl@users.noreply.github.com>
2021-04-21 18:17:14 +0300

    settings

--- settings.js
+++ settings.js
@@ -2,29 +2,35 @@ var leftKey = 37;
 var upKey = 38;
 var rightKey = 39;
 var downKey = 40;
-var numOfBalls = 70;
 var color5PointsBall = "#e66465";
 var color15PointsBall = "FF7B33";
 var color25PointsBall = "#f6b73c"; 
 var gameTime = 150;
 var numOfMonsters = 1
 
-function setKey(key, code) {
-    console.log(key, code)
-    switch (key) {
-        case left:
-            leftKey = code;
-        case up:
-            upKey = code;
-        case right:
-            rightKey = code;
-        case down:
-            downKey = code;
+function setKey(key, e) {
+    //TODO: fix
+    let code = e.keyCode;
+    if (47<code<106){
+        console.log(key, code)
+        switch (key) {
+            case 'left':
+                leftKey = code;
+            case 'up':
+                upKey = code;
+            case 'right':
+                rightKey = code;
+            case 'down':
+                downKey = code;
+        }
     }
 }
 
 function setBallsNum(num) {
-    numOfBalls = num;
+    food_remain = num;
+    numOf5PointsBall = Math.ceil(0.6*num);
+    numOf15PointsBall = Math.ceil(0.3*num);
+    numOf25PointsBall = Math.ceil(0.1*num);
 }
 
 function setballsColor(points, color) {
@@ -48,5 +54,29 @@ function setNumOfMonsters(num) {
 }
 
 function setRandomSettings() {
+    setBallsNum(Math.floor(Math.random()*41)+50);
+    setballsColor(5, setRandomColor());
+    setballsColor(15, setRandomColor());
+    setballsColor(25, setRandomColor());
+    setgameTime(Math.floor(Math.random()*241)+60);
+    setNumOfMonsters(Math.floor(Math.random()*4)+1);
+}
+
+function setRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var hashtag = '#';
+    for (var i = 0; i < 6; i++) {
+        randomColor = hashtag + letters[Math.floor(Math.random() * 16)];
+    }
+    return randomColor; 
+}
+
+function play() {
+    $(document).ready(function() {
+        context = canvas.getContext("2d");
+        Start();
+    });
+    show(settings, app);
+
 
 }
