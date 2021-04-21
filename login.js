48558a87c1dd84a10ff60aea1e69d6f99b040a1f
noaakl <62835962+noaakl@users.noreply.github.com>
2021-04-21 15:50:06 +0300

    login and settings

--- login.js
+++ login.js
@@ -1,21 +1,21 @@
-import allUsers from register.js
-
 function logIn() {
-    // if (isValidLogin()) {
-    //     getElementById("loginForm").reset();
-    //     show(login, settings);
-    // }
-    // else {
-    //     alert("Wrong username or password. Please try again")
-    // }
-    alert("Please")
+    if (isValidLogin()) {
+        loginForm.reset();
+        show(login, settings);
+    }
+    else {
+        alert("Wrong username or password. Please try again")
+    }
 }
 
 function isValidLogin() {
-    let userName = getElementById("loginForm").elements[0].value;
-    let password = getElementById("loginForm").elements[1].value;
-    for (const user in allUsers) {
-        if (user.userName === userName && user.password === password) {
+    let userName = loginForm.elements[0].value;
+    let password = loginForm.elements[1].value;
+    console.log(userName, password)
+    console.log(allUsers)
+    for (let i = 0; i < allUsers.length; i++) {
+        console.log(allUsers[i].userName)
+        if (allUsers[i].userName === userName && allUsers[i].password === password) {
             return true;
         }
     }
